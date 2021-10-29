f984a73c4bb3a7a169800c2832dd5b1c4338c685
pervejislam2404 <pervejislam2404@gmail.com>
2021-10-30 01:06:27 +0600

    update

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function run() {
         app.delete('/cancel/:id', async(req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await AddedService.findOne(query);
+            const result = await AddedService.deleteOne(query);
             res.json(result);
         })
 
